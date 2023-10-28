f5fad9cdc30c8ad1d97fa5304388f1c563a067e0
Corsolaa🍎 <bruno.bouwman4@gmail.com>
2023-10-29 01:37:25 +0200

    Added image preview feature.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("I can smell you...");
+const image_input = document.querySelector(".image_input");
+const image_demo = document.querySelector(".image_demo");
+
+image_input.addEventListener("change", () => {
+   const [file] = image_input.files;
+    if (file) {
+        image_demo.src = URL.createObjectURL(file)
+    }
+});
