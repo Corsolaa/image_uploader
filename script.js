7cb412cf5a062ff6033db6e7f39c19db79738a2b
Corsolaa🍎 <bruno.bouwman4@gmail.com>
2023-10-31 13:02:54 +0100

    Changed animation and timing. Changed animation to opacity and fiddled with the timing.

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ const image_input = document.querySelector(".image_input");
 const image_label = document.querySelector(".image_label");
 const image_demo = document.querySelector(".image_demo");
 
-image_label.addEventListener("click", () => {
-    image_demo.classList.remove("expand");
-});
-
 image_input.addEventListener("change", () => {
-   const [file] = image_input.files;
-    if (file) {
-        image_demo.src = URL.createObjectURL(file);
-        setTimeout(() => {
-            image_demo.classList.add("expand");
-        }, 500);
-    }
+    image_demo.classList.remove("visible");
+    setTimeout(() => {
+        const [file] = image_input.files;
+        if (file) {
+            image_demo.src = URL.createObjectURL(file);
+            setTimeout(() => {
+                image_demo.classList.add("visible");
+            }, 100);
+        }
+    }, 300);
 });
